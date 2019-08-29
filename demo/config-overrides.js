110f9d06849c8e73a4574b1345610a500ccdebc5
LucDomingo <47193801+LucDomingo@users.noreply.github.com>
2019-08-29 17:05:31 +0200

    Update config-overrides.js

--- demo/config-overrides.js
+++ demo/config-overrides.js
@@ -1,10 +1,3 @@
-// Using react-app-rewired to tweak the create-react-app webpack config(s) without using 'eject' and
-// without creating a fork of the react-scripts.
-//
-// This is needed for styled-components (react-app-rewire-styled-components)
-// but is generally useful if we want to add other configurations in the future.
-// see: https://github.com/timarney/react-app-rewired
-
 const rewireStyledComponents = require('react-app-rewire-styled-components');
 
 module.exports = function override(config, env) {
