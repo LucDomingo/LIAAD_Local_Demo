58a0c707611314a5ef4a366453702968e3eebf53
LucDomingo <47193801+LucDomingo@users.noreply.github.com>
2019-09-26 10:49:35 +0200

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -17,13 +17,9 @@ import './css/Accordion.css';
 import './css/hierplane-overrides.css';
 import './css/visualization-types.css';
 
-/*******************************************************************************
-  <App /> Container
-*******************************************************************************/
+
 
 const DEFAULT_PATH = "/PAMPO"
-
-// The App is just a react-router wrapped around the Demo component.
 const App = () => (
   <Router>
     <div>
@@ -39,7 +35,6 @@ class Demo extends React.Component {
   constructor(props) {
     super(props);
 
-    // React router supplies us with a model name and (possibly) a slug.
     const { model, slug } = props.match.params;
 
     this.state = {
@@ -47,24 +42,15 @@ class Demo extends React.Component {
       selectedModel: model,
       requestData: null,
       responseData: null,
-      expandedModelGroupIndexes: [0, 1, 2, 3, 4] // expand first five by default
+      expandedModelGroupIndexes: [0, 1, 2, 3, 4] 
     };
 
-    // We'll need to pass this to the Header component so that it can clear
-    // out the data when you switch from one model to another.
     this.clearData = () => {
       this.setState({requestData: null, responseData: null})
     }
-
-    // preserve open menu state
     this.handleExpandModelGroup = (expandedModelGroupIndexes) => {
       this.setState({ expandedModelGroupIndexes: expandedModelGroupIndexes });
     };
-
-    // Our components will be using history.push to change the location,
-    // and they will be attaching any `requestData` and `responseData` updates
-    // to the location object. That means we need to listen for location changes
-    // and update our state accordingly.
     props.history.listen((location, action) => {
       const { state } = location;
       if (state) {
@@ -74,21 +60,14 @@ class Demo extends React.Component {
     });
   }
 
-  // We also need to update the state whenever we receive new props from React router.
   componentWillReceiveProps({ match }) {
     const { model, slug } = match.params;
     this.setState({selectedModel: model, slug: slug});
   }
-
-  // After the component mounts, we check if we need to fetch the data
-  // for a permalink.
   componentDidMount() {
     const { slug, responseData } = this.state;
 
-    // If this is a permalink and we don't yet have the data for it...
     if (slug && !responseData) {
-      // Make an ajax call to get the permadata,
-      // and then use it to update the state.
       fetch(`${API_ROOT}/permadata`, {
         method: 'POST',
         headers: {
@@ -113,7 +92,6 @@ class Demo extends React.Component {
 
     const ModelComponent = () => {
       if (slug && !responseData) {
-        // We're still waiting for permalink data, so just return the placeholder component.
         return (<WaitingForPermalink/>)
       } else if (modelComponents[selectedModel]) {
           // This is a model we know the component for, so render it.
